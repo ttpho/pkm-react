fb6706fd2dd63f6b79f7ff3dd6587de9db09576f
pho.tran <tranthanhpho2011@gmail.com>
2020-07-20 23:22:47 +0700

    remove json on local

--- src/PokemonGrid/index.js
+++ src/PokemonGrid/index.js
@@ -1,27 +1,62 @@
 import React from 'react';
-import pokemon_data from './pokemons.json';
 import { StyledPokemonGrid } from './style';
 import PokemonItem from '../PokemonItem';
 
-function PokemonGrid() {
-    return (
-        <StyledPokemonGrid>
-            {
-                pokemon_data.results
-                    // remove duplicated item
-                    .reduce((unique, o) => {
-                        if (!unique.some(obj => obj.name === o.name)) {
-                            unique.push(o);
+
+class PokemonGrid extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            results: [],
+            isLoading: true
+        };
+    }
+
+    componentDidMount() {
+        let url = "https://raw.githubusercontent.com/ttpho/Pokemon-Pocket/master/datas/pokemons.json"
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({ results: json.results });
+            })
+            .catch((error) => console.error(error))
+            .finally(() => {
+                this.setState({ isLoading: false });
+            });
+    }
+
+    render() {
+        const { results, isLoading } = this.state;
+
+        return (
+            <div style={{ flex: 1, padding: 24 }}>
+                {isLoading ? <h1>Loading...</h1> : (
+                    <StyledPokemonGrid>
+                        {
+                            // remove duplicated item
+                            results
+
+                                .reduce((unique, o) => {
+                                    if (!unique.some(obj => obj.name === o.name)) {
+                                        unique.push(o);
+                                    }
+                                    return unique;
+                                }, [])
+                                // create item 
+                                .map((item) => {
+                                    return <PokemonItem pokemon={item} />
+                                })
                         }
-                        return unique;
-                    }, [])
-                    // create item 
-                    .map((item) => {
-                        return <PokemonItem pokemon={item} />
-                    })
-            }
-        </StyledPokemonGrid>
-    );
+                    </StyledPokemonGrid>
+
+
+                )}
+            </div>
+        );
+    }
+
 }
+
 
 export default PokemonGrid;
