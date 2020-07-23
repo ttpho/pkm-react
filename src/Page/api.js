4f1ad64932dbd498a12aec045be1fe7d4b92b896
Pho Tran <tranthanhpho2011@gmail.com>
2020-07-23 21:12:32 +0700

    make read JSON faster

--- src/Page/api.js
+++ src/Page/api.js
@@ -1,13 +1,8 @@
 export async function getPokemonsAsync() {
-    const url = "https://raw.githubusercontent.com/ttpho/Pokemon-Pocket/master/datas/pokemons.json"
+    const url = "https://raw.githubusercontent.com/ttpho/pkm-react/master/pokemon_json.json/pkms.json"
     let response = await fetch(url);
     let json = await response.json();
-    return json.results.reduce((unique, o) => {
-        if (!unique.some(obj => obj.name === o.name)) {
-            unique.push(o);
-        }
-        return unique;
-    }, []);
+    return json;
 };
 
 export default getPokemonsAsync;
